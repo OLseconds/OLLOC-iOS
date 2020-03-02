ee91b415308119a541ec9e31d97185e128c209ef
bell2lee <paperlee@kookmin.ac.kr>
2020-03-03 01:58:43 +0900

    Initial Commit

---
+++ olloc/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  olloc
+//
+//  Created by 이종휘 on 2020/03/03.
+//  Copyright © 2020 OLSconds. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
